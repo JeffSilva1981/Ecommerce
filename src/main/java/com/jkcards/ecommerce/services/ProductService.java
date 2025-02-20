be349ba53beaba64104059d55b367f94a2926801
JeffSilva1981 <jeff.contactwork@gmail.com>
2025-02-20 15:21:30 -0300

    GET Product Paged

--- src/main/java/com/jkcards/ecommerce/services/ProductService.java
+++ src/main/java/com/jkcards/ecommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.jkcards.ecommerce.dto.ProductDTO;
 import com.jkcards.ecommerce.entities.Product;
 import com.jkcards.ecommerce.repositories.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,6 +24,13 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
+
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x-> new ProductDTO(x));
 
     }
 
