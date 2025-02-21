6c5e32bc6219a66fc04b88b7ab905383fae3bec4
JeffSilva1981 <jeff.contactwork@gmail.com>
2025-02-21 00:23:01 -0300

    DELETE Product

--- src/main/java/com/jkcards/ecommerce/services/ProductService.java
+++ src/main/java/com/jkcards/ecommerce/services/ProductService.java
@@ -48,7 +48,10 @@ public class ProductService {
         copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new ProductDTO(entity);
-
+    }
+    @Transactional
+    public void delete(Long id){
+        repository.deleteById(id);
     }
 
     private void copyDtoToEntity(ProductDTO dto, Product entity) {
